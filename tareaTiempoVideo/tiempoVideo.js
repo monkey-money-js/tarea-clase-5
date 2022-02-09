a0d3805b3a6ce81b554cd0c3c9f1008832979786
Santiago Chamorro <santychamorro29@gmail.com>
2022-02-08 21:53:19 -0300

    Tiempo total de video.
    Este commit permite al usuario ingresar la cantidad de tiempo de 5 videos y calcular el total.

---
+++ tareaTiempoVideo/tiempoVideo.js
@@ -0,0 +1,46 @@
+const arrayHoras = document.querySelectorAll('.horas');
+const arrayMinutos = document.querySelectorAll('.minutos');
+const arraySegundos = document.querySelectorAll('.segundos');
+
+const $botonCalcular = document.querySelector('.boton-calcular');
+$botonCalcular.onclick = function(){
+    calcularTiempo();
+
+
+    return false;
+}
+
+function calcularTiempo(){
+    
+    let totalHoras = 0;
+    for (let i=0; i<5; i++){
+        totalHoras += Number(arrayHoras[i].value);
+    }
+
+    let totalMinutos = 0;
+    for (let i=0; i<5; i++){
+        totalMinutos += Number(arrayMinutos[i].value);
+        if (totalMinutos > 60){
+            totalHoras++;
+            totalMinutos -= 60;
+        }
+    }
+
+    let totalSegundos = 0;
+    for (let i=0; i<5; i++){
+        totalSegundos += Number(arraySegundos[i].value);
+        if (totalSegundos > 60){
+            totalMinutos++;
+            totalSegundos -= 60;
+        }
+    }
+
+    mostrarInfo(totalHoras,totalMinutos,totalSegundos);
+    
+}
+
+function mostrarInfo(totalHoras,totalMinutos,totalSegundos){
+    const $resultado = document.querySelector('#resultado');
+    $resultado.textContent = `El tiempo total de videos es de ${totalHoras} horas, ${totalMinutos} minutos y ${totalSegundos} segundos`;
+}
+    
